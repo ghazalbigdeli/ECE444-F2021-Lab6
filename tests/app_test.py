1ca3e4c5026bbc8f0bfcfc18e169264886b5c6f6
ghazalbigdeli <ghazal.bigdeli@gmail.com>
2021-10-31 17:55:00 -0400

    Completed Part 1

--- tests/app_test.py
+++ tests/app_test.py
@@ -20,7 +20,6 @@ def client():
     db.drop_all()  # teardown
 
 
-
 def login(client, username, password):
     """Login helper function"""
     return client.post(
@@ -75,7 +74,8 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
-    
+
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get("/delete/1")
@@ -85,4 +85,3 @@ def test_delete_message(client):
     rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
-
